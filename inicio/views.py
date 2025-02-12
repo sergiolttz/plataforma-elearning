752a5a2e36ef0304799d76c484ca65b814e0831e
sergiolttz <sergiolattanzio05@gmail.com>
2025-02-12 13:02:22 -0400

    falta vista sala entrenamiento y templates

--- inicio/views.py
+++ inicio/views.py
@@ -1,9 +1,54 @@
 from django.shortcuts import render
 
+from django.shortcuts import HttpResponse, HttpResponseRedirect
+from .models import Usuario
+
 # Create your views here.
 
 def pagina_inicio(request):
-    pass
+    return render(request, "inicio.html")
 
 def registro_login(request):
-    pass
+    entrada = request.POST.get('entrada').split()
+
+    if len(entrada)==4:
+        cont=0
+
+        for i in entrada:
+            if entrada.count(i) == 2 and len(i)>7:
+                contraseña = i; cont +=1
+                print("la contraseña es: ", {contraseña})
+            
+            if ("@" in i) and (entrada.count(i)==1):
+                correo = i; cont +=1
+            
+            if not "@" in i and (entrada.count(i)==1):
+                nombre_de_usuario = i; cont +=1
+
+        if cont == 4:
+            usuario_nuevo = Usuario()
+            usuario_nuevo.nombre_de_usuario = nombre_de_usuario
+            usuario_nuevo.correo = correo
+            usuario_nuevo.contraseña = contraseña
+            usuario_nuevo.save()
+            return render(request, "inicio.html")
+
+    elif len(entrada)==2:
+        try:
+            print(entrada[0])
+            usuario_ingresado = Usuario.objects.get(nombre_de_usuario=entrada[0])
+        except:
+            try:
+                print (entrada[1])
+                usuario_ingresado = Usuario.objects.get(nombre_de_usuario=entrada[1])
+            except:
+                print("ERROR")
+                return HttpResponse("No ha respetado el formato indicaro, vuelva a intentarlo")
+        
+        if usuario_ingresado.contraseña == entrada[0] or usuario_ingresado.contraseña==entrada[1]:
+            archivo = open("usuario.txt","w")
+            archivo.write(str(usuario_ingresado.nombre_de_usuario))
+            archivo.close()
+            return HttpResponseRedirect("/perfil/mostrar_perfil/")
+        
+    return HttpResponse("No ha respetado el formato indicado, vuelva a intentarlo")          
--- perfil/views.py
+++ perfil/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect
+from inicio.models import Usuario
 
 # Create your views here.
 
+def obtener_usuario():
+    global usuario_ingresado
+    archivo = open("usuario.txt", "r")
+    usuario_ingresado = Usuario.objects.get(nombre_de_usuario=archivo.readLine())
+    archivo.close()
+
 def mostrar_perfil(request):
-    pass
+    obtener_usuario()
+    nombre_completo = usuario_ingresado.nombre + " " + usuario_ingresado.apellido
+
+    return render(request, "perfil.html",{
+        "nombre_usuario": usuario_ingresado.nombre_de_usuario,
+        "puntuacion": usuario_ingresado.puntuacion_c_mas_mas+usuario_ingresado.puntuacion_python+usuario_ingresado.puntuacion_go,
+        "puntos_c":usuario_ingresado.puntuacion_c_mas_mas,
+        "puntos_py":usuario_ingresado.puntuacion_python,
+        "puntos_go":usuario_ingresado.puntuacion_go,
+        "nombre": nombre_completo if nombre_completo else "--",
+        "link": usuario_ingresado.url if usuario_ingresado.url else "--",
+        "text": usuario_ingresado.sobre_mi if usuario_ingresado.sobre_mi else "--",
+        })
 
 def agregar_info(request):
     pass
